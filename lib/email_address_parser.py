12dbca034a980442184cb6546c5be7791ed0df02
matthew rendely <matthew.rendely@gmail.com>
2023-06-19 18:00:26 -0700

    done

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
-# your code goes here!
+import re 
+
+class EmailAddressParser():
+
+    email_match_pattern = r"[A-z]+[A-z|0-9|.]+\@[A-z|0-9|.]+"
+    email_regex = re.compile(email_match_pattern)
+
+    def __init__(self, emails_string):
+        self.emails_string = emails_string
+
+    def parse(self):
+        email_list = EmailAddressParser.email_regex.findall(self.emails_string)
+        return sorted(email_list)
